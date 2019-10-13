ce8efa4e2395ef6cb6a238aafc32cf6b94dba13f
Catori Christopher <catori18@hotmail.com>
2019-10-13 16:07:51 -0400

    Created JSON Object to parse

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -1,13 +1,20 @@
 var apikey = 'd26f1df15f62dee44b7f5d7d6dfc2474';
 var requestURL = '//api.openweathermap.org/data/2.5/weather';
 var requestQuery = '?zip=94040,us&APPID=' + apikey; 
-console.log(requestURL + requestQuery);
+var weatherObject = new XMLHttpRequest();
 
 // Initialiize XMLHttpRequest weather object
- var weatherObject = new XMLHttpRequest();
+
  weatherObject.open('GET', requestURL + requestQuery, true);
  weatherObject.send();
 
  //Convert response text to JSON object
- console.log(weatherObject.responseText);
+ weatherObject.onload = function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+
+ }
+ 
  
